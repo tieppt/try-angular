e757fc584b51ae6ba6d25c61428b4662b6fc30df
Tiep Phan <tieppt12@gmail.com>
2017-05-21 18:06:24 +0700

    done lesson 17

--- contact-ngf/src/app/contact-form/contact-form.component.ts
+++ contact-ngf/src/app/contact-form/contact-form.component.ts
@@ -6,14 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
+  contact = {
+    "contactName": "Tiep Phan",
+    "email": "abc@deg.com",
+    "facebook": "facebook.com",
+    "twitter": "twitter.com",
+    "website": "tiepphan.com",
+    "tel": "1234-5678-90"
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onSubmit() {
+  onSubmit(formValue) {
     // Do something awesome
+    console.log(formValue);
+    // throw Error('something go wrong')
   }
 
 }
